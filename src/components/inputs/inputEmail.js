018440d8d1b520c5ff513e603a9e2e535800dbbe
Nour99-ai <nm2012k@gmail.com>
2025-06-18 15:48:09 +0200

    Initial commit

---
+++ src/components/inputs/inputEmail.js
@@ -0,0 +1,10 @@
+let InputEmail = () => {
+  return (
+    <div>
+      <input placeholder={"Enter your Email"} className={"inputEmail"} />
+    </div>
+  );
+};
+
+export default InputEmail;
+
---
+++ src/components/inputs/inputPass.js
@@ -0,0 +1,8 @@
+let InputPass = () => {
+  return (
+    <div>
+      <input className={"inputEmail"} placeholder="Enter your Password" />
+    </div>
+  );
+};
+export default InputPass;
