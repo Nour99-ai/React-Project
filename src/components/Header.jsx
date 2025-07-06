099a9de97f043f9f53675bb57041fd4299a05078
Nour99-ai <nm2012k@gmail.com>
2025-07-06 23:35:23 +0200

    Adding the LogInInputs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="bg-gradient-to-r from-red-500 via-pink-500 to-blue-500 py-10 px-10 font-semibold text-2xl text-center rounded-lg max-w-4xl mx-6 my-6 md:my-8 md:mx-14 md:py-12 md:font-bold lg:mx-auto lg:text-3xl xl:max-w-screen-xl xl:my-12 2xl:max-w-full 2xl:mx-20 2xl:py-16">
+        <div className="bg-gradient-to-r from-red-500 via-pink-500 to-blue-500 py-10 px-10 font-semibold text-2xl text-center rounded-lg max-w-4xl mx-6 my-6 md:my-8 md:mx-14 md:py-12 md:font-bold lg:mx-auto lg:text-3xl xl:max-w-screen-xl xl:my-12 2xl:max-w-full 2xl:mx-20 2xl:py-16 2xl:text-5xl">
             <h1 className="text-slate-200">Welcome to Nour Web</h1>
         </div>
     );
